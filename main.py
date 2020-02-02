d5067863a9e8801b86cc9e4fe9f5fec1d421ae66
Jia Rong Chua <jiarongchua92@gmail.com>
2020-02-01 19:22:16 -0500

    updated files

--- main.py
+++ main.py
@@ -24,13 +24,13 @@ df_moma_1 = pd.read_csv(rel_to_cwd_path_1, header = 0, index_col = 0)
 
 # assuming that both have the same number of entities/topics
 
-moma_dept = df_moma_1["Department"].unique()
+moma_dept = df_moma_1["Department"].unique()[:-2]
 
 app.layout = html.Div([
-    html.H1('The Moma Collection'),
+    html.H1('The MoMa Collection'),
     html.Div([
         html.Div([
-            html.P("Filter by Department"),
+            html.P("Filter by Department:"),
             dcc.Dropdown(
                 id='dept-selection',
                 options=[{'label': i, 'value': i} for i in moma_dept],
@@ -38,7 +38,15 @@ app.layout = html.Div([
             )
         ])
     ]),
-        dcc.Graph(id = 'moma-dept-time-chart')
+        dcc.Graph(id = 'moma-dept-time-chart'),
+        html.Div([
+            html.Div([
+                dcc.Graph(id = 'moma-dept-dim-chart'),
+            ], className = 'six columns'),
+            html.Div([
+                dcc.Graph(id = 'moma-dept-artist-chart')
+            ], className = 'six columns')
+        ], className = 'row')
 ])
 
 @app.callback(
@@ -48,7 +56,7 @@ app.layout = html.Div([
 def update_dept_time(dept):
     dff_dept_pre = df_moma_1[df_moma_1['Department'] == dept]
     dff_dept_pre.dropna(subset = ['Acquisition Date'], inplace = True)
-    dff_dept_pre['Year'] = pd.to_datetime(dff_dept_pre['Acquisition Date']).apply(lambda x: x.year)
+    dff_dept_pre['Year'] = pd.to_datetime(dff_dept_pre['Acquisition Date'], errors = 'coerce').apply(lambda x: x.year)
     dff_dept = dff_dept_pre.groupby('Year')['Title'].count()
     dff_dept = pd.DataFrame(dff_dept)
     return({
@@ -62,10 +70,59 @@ def update_dept_time(dept):
                 )
             )],
         'layout': go.Layout(
-            barmode = 'stack',
             title = 'Number of Art Pieces by Year of Creation: ' + str(dept),
             xaxis = {'title': 'dates'},
             yaxis = {'title': 'art pieces created'}
+        )
+    })
+
+@app.callback(
+    Output('moma-dept-dim-chart', 'figure'),
+    [Input('dept-selection', 'value')])
+ 
+def update_dept_dim(dept):
+    dff_dept = df_moma_1[df_moma_1['Department'] == dept]
+    return({
+        'data': [
+            go.Scatter(
+                x = dff_dept['Width (cm)'], 
+                y = dff_dept['Height (cm)'],
+                mode = 'markers',
+                name = 'data',
+                marker = dict(  
+                    color = "blue"
+                )
+            )],
+        'layout': go.Layout(
+            title = 'Art Pieces by Width (cm) and Height (cm): ' + str(dept),
+            xaxis = {'title': 'Width (cm)'},
+            yaxis = {'title': 'Height (cm)'}, 
+            font = dict(size = 8)
+        )
+    })
+
+@app.callback(
+    Output('moma-dept-artist-chart', 'figure'),
+    [Input('dept-selection', 'value')])
+ 
+def update_dept_artist(dept):
+    dff_dept_pre = df_moma_1[df_moma_1['Department'] == dept]
+    dff_dept_artist = dff_dept_pre['Name'].value_counts()[:20]
+    return({
+        'data': [
+            go.Bar(
+                x = dff_dept_artist.index, 
+                y = dff_dept_artist.values,
+                name = 'count',
+                marker = dict(  
+                    color = "orange"
+                )
+            )],
+        'layout': go.Layout(
+            title = 'Top 20 Artists: ' + str(dept),
+            xaxis = {'title': 'Artists'},
+            yaxis = {'title': 'Number of Art Pieces'}, 
+            font = dict(size = 8)
         )
     })
 
